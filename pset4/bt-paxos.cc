5ec80743d2c34c03cebb6e6ac81036d768af8b00
Ethan Carter Edwards <ethan@ethancedwards.com>
2026-05-11 18:49:28 -0400

    peer add logic

--- pset4/bt-paxos.cc
+++ pset4/bt-paxos.cc
@@ -41,6 +41,18 @@ bool copy_exact(char (&field)[N], std::string_view value) {
     return true;
 }
 
+bt_info_hash make_info_hash(const char (&bytes)[20]) {
+    bt_info_hash key;
+    memcpy(key.data(), bytes, key.size());
+    return key;
+}
+
+bt_peer_id make_peer_id(const char (&bytes)[20]) {
+    bt_peer_id key;
+    memcpy(key.data(), bytes, key.size());
+    return key;
+}
+
 std::optional<uint32_t> parse_ipv4(std::string_view value) {
     std::string dotted(value);
     dotted += "."; // hack
@@ -181,8 +193,32 @@ void parse_announce_request(cot::http_message& req,
     }
 }
 
+void apply_announce(bt_tracker_state& state, const bt_tracker_announce_request& bt_req) {
+    bt_info_hash info_hash = make_info_hash(bt_req.info_hash);
+    bt_peer_id peer_id = make_peer_id(bt_req.peer_id);
+    auto& peers = state.torrents[info_hash];
+
+    if (bt_req.event == stopped) {
+        peers.erase(peer_id);
+        return;
+    }
+
+    bt_peer peer;
+
+    peer.peer_id = peer_id;
+    peer.ip = bt_req.ip;
+    peer.port = bt_req.port;
+    peer.uploaded = bt_req.uploaded;
+    peer.downloaded = bt_req.downloaded;
+    peer.left = bt_req.left;
+    peer.complete = bt_req.event == completed || bt_req.left == 0;
+
+    peers[peer_id] = peer;
+}
+
 cot::task<> run_server() {
     printf("Running the server\n");
+    bt_tracker_state tracker_state;
     cot::fd lfd = co_await cot::tcp_listen("127.0.0.1:9000");
     cot::fd cfd = co_await cot::tcp_accept(lfd);
 
@@ -218,6 +254,8 @@ cot::task<> run_server() {
                     .header("Content-Type", "text/plain")
                     .body(failure);
             } else {
+                apply_announce(tracker_state, bt_req);
+
                 res.status_code(200)
                     .header("Content-Type", "text/plain")
                     .body();
