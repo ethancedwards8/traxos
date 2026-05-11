7b73b2394c59c88587884a8313d1f40fc67a372c
Ethan Carter Edwards <ethan@ethancedwards.com>
2026-05-10 20:12:59 -0400

    state tracking for torrent, peers

--- pset4/bittorrent.hh
+++ pset4/bittorrent.hh
@@ -1,10 +1,18 @@
 #pragma once
+#include <array>
+#include <cstdint>
+#include <map>
 
 enum bt_event {
     started,
     stopped,
     completed
 };
+
+// https://stackoverflow.com/questions/60136080/using-character-array-as-a-key-in-map?rq=3
+// std::array over char[N] because of std::map struggles
+using bt_info_hash = std::array<char, 20>;
+using bt_peer_id = std::array<char, 20>;
 
 // https://wiki.theory.org/BitTorrent_Tracker_Protocol#Basic_Tracker_Announce_Request
 struct bt_tracker_announce_request {
@@ -25,4 +33,18 @@ struct bt_tracker_announce_request {
     bt_event event;
 
     int32_t numwant = 50;
+};
+
+struct bt_peer {
+    bt_peer_id peer_id;
+    uint32_t ip = 0;
+    uint16_t port = 0;
+    uint64_t uploaded = 0;
+    uint64_t downloaded = 0;
+    uint64_t left = 0;
+    bool complete = false;
+};
+
+struct bt_tracker_state {
+    std::map<bt_info_hash, std::map<bt_peer_id, bt_peer>> torrents;
 };
