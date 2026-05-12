4daf3f9669b06bcbc355861cdff6d2dfbfc8bcf3
Ethan Carter Edwards <ethan@ethancedwards.com>
2026-05-12 18:56:02 -0400

    working test.sh

---
+++ pset4/test.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+set -x
+
+RND="${1:-20}"
+
+build/traxos-paxos-test -R "${RND}"
+build/traxos-paxos-test --loss 0.05 -R "${RND}"
+build/traxos-paxos-test --failure-mode failed_leader -R "${RND}"
+build/traxos-paxos-test --failure-mode failed_replica --failed-replica 1 -R "${RND}"
+build/traxos-paxos-test --failure-mode failed_replica --failed-replica 2 -R "${RND}"
